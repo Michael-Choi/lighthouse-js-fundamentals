f48229d17f39081809d0bd7fd3687bb9f01734e5
Michael-Choi <michael.choi@queensu.ca>
2019-07-14 13:46:13 -0400

    added array icecreamflavours task

---
+++ icecreamflavors.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//first icecream flavour
+console.log(iceCreamFlavours[0]);
+
+//last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//total flavours
+console.log(iceCreamFlavours.length);
